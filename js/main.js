c18c71aa9b2fa1536ee6010444d166a1516c2585
welberthenrique <welberthenrique@gmail.com>
2021-09-10 10:45:32 -0300

    Organizaçao da estrutura

--- js/main.js
+++ js/main.js
@@ -1,82 +1,8 @@
-//05:41:00  trabalhando com funçoes no curso do kasolution
-
-
-// let binario = parseInt('11111111'); utilizar essa variavel para binario. criarei outra apenas para teste
-
-
-
-
 var botao = document.getElementById("calcular");
 botao.addEventListener('click', function(){
                                    var resultado = leDados;
                                     mostrarResultado(entradaBinario.value);
                                 });
-
-
-// console.log(binario);
-// let elBinario = document.getElementById("saidaBinario");
-// elBinario.innerHTML = binario;
-// let elDecimal = document.getElementById("saidaDecimal");
-// elDecimal.innerHTML = decimal;
-
-
-
-// clicou = function (inputEntrada){
-//  inputEntrada = document.getElementById("entrada");
-
-// return inputEntrada;
-// }
-
-// var teste = clicou(inputEntrada);
-// console.log(teste);
-
-
-// let valorEntrada = document.getElementById("entrada").addEventListener('click', clicou());
-// console.log(valorEntrada);
-
-// let convertido = valorEntrada *2;
-// let restultadoNaTela = document.getElementById("resultado");
-
-  
-// restultadoNaTela.innerHTML = `<h4> Resultado: ${convertido}</h4>`;
-
-
-
-
-
-
-
-// function calculo(entrada){
-//     let resultado = lerEntrada();
-//     resultado = resultado.value +1;
-
-//     // console.log(resultado);
-//     return resultado;
-// }
-
-// function lerEntrada(){
-
-//     let inputEntrada = document.getElementById("entrada");
-//     // inputEntrada = entrada.value;
-//     console.log(inputEntrada.value);
-//     return inputEntrada.value;
-// }
-
-// function gerarResultado(resultado){
-//     let restultado = document.getElementById("resultado");
-  
-//     let valor = calculo(restultado);
-//     restultado.innerHTML = `<h4> Resultado: ${valor}</h4>`;
-    
-// }
-
-
-// var botaoConverter = document.getElementById("calcular");
-// let inputEntrada = lerEntrada();
-// console.log(inputEntrada);
-// botaoConverter.addEventListener('click', function(){
-//                                 gerarResultado(inputEntrada);
-// })
 
 
 
